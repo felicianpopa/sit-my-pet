ca9586680715a60a2b1d4173fed3a0bc7f7b6ac6
Felician Popa <felician.popa@freshbyteinc.com>
2017-06-22 14:21:19 +0300

    Moved functionality from the main controller to the processUserData service.

--- js/controllers/MainCtrl.js
+++ js/controllers/MainCtrl.js
@@ -1,4 +1,5 @@
-app.controller('mainCtrl', ['$scope', '$location', '$http', '$ocModal', 'mainDataService',  function($scope, $location, $http, $ocModal, mainDataService){
+app.controller('mainCtrl', ['$scope', '$location', '$http', '$ocModal', 'mainDataService', 'processUserData',
+	function($scope, $location, $http, $ocModal, mainDataService, processUserData){
 	$scope.windowLoaded = function() {
 		console.log('window loaded');
 	}
@@ -20,58 +21,23 @@ app.controller('mainCtrl', ['$scope', '$location', '$http', '$ocModal', 'mainDat
 			controller: 'mainCtrl'
 		});
 	}
-	$scope.logOut = function() {
-		$scope.mainData.user.loggedIn = false;
+
+	$scope.addNewUser = function(newUserData) {
+		processUserData.addNewUser(newUserData, $scope);
 	}
+
 	$scope.logIn = function() {
-		console.log('running')
-		if(angular.equals($scope.mainData.usersList, {})) {
-			alert('nu exista nici un user');
-			$ocModal.close();
-		}
-		else {
-			for(var key in $scope.mainData.usersList) {
-				if ($scope.mainData.usersList[key].userName == jQuery('input[name="userName"]').val()){
-					if ($scope.mainData.usersList[key].userPassword == jQuery('input[name="userPassword"]').val()) {
-						$ocModal.close();
-						$scope.mainData.user.loggedIn = true;
-						$scope.mainData.user.loggedInUserName = jQuery('input[name="userName"]').val();
-						mainDataService.saveMainData($scope.mainData);
-						return
-					}
-					else {
-						alert('password is incorrect')
-					}
-				}
-				else {
-					alert('user is incorrect')
-				}
-			}
-		}
+		processUserData.logIn($scope);
 	}
+
+	$scope.logOut = function() {
+		processUserData.logOut($scope);
+	}
+
 	// Call the login function.
 	jQuery(document).on('click', '#login', function(){
 		$scope.logIn();
 	});
-	// Add a new user
-	$scope.addNewUser = function(newUser) {
-		if($scope.newUser.userPassword !== $scope.newUser.confirmUserPassword) {
-			$scope.newUserForm.$valid = false;
-			alert('parola confirmata nu este aceeasi cu parola, va ruga sa le recompletati');
-			$scope.newUser.userPassword = null;
-			$scope.newUser.confirmUserPassword = null;
-
-		}
-		if($scope.newUserForm.$valid) {
-			$scope.formNotValid = false;
-			var newUserName = $scope.newUser.userName
-			$scope.mainData.usersList[newUserName] = newUser;
-		 	mainDataService.saveMainData($scope.mainData);
-		}
-		else{
-			$scope.formNotValid = true;
-		}
-	}
 
 	mainDataService.loadMainData().then(function(response){
 		$scope.mainData = response.data
--- js/services/mainServices.js
+++ js/services/mainServices.js
@@ -11,3 +11,62 @@ app.service('mainDataService', function($http){
 	    });
 	}
 });
+
+app.service('processUserData',['mainDataService', '$ocModal', function(mainDataService, $ocModal){
+	this.addNewUser = function(newUser, $scope) {
+		if($scope.newUser.userPassword !== $scope.newUser.confirmUserPassword) {
+			$scope.newUserForm.$valid = false;
+			alert('parola confirmata nu este aceeasi cu parola, va ruga sa le recompletati');
+			$scope.newUser.userPassword = null;
+			$scope.newUser.confirmUserPassword = null;
+
+		}
+		if($scope.newUserForm.$valid) {
+			$scope.formNotValid = false;
+			var newUserName = $scope.newUser.userName
+			$scope.mainData.usersList[newUserName] = newUser;
+			mainDataService.saveMainData($scope.mainData);
+		}
+		else{
+			$scope.formNotValid = true;
+		}
+	}
+	this.logIn = function($scope) {
+		var userFound = false;
+		var passwordCorrect = false;
+		if(angular.equals($scope.mainData.usersList, {})) {
+			alert('nu exista nici un user');
+			$ocModal.close();
+		}
+		else {
+			for(var key in $scope.mainData.usersList) {
+				if ($scope.mainData.usersList[key].userName == jQuery('input[name="userName"]').val()){
+					userFound = true
+
+					if ($scope.mainData.usersList[key].userPassword == jQuery('input[name="userPassword"]').val()) {
+						passwordCorrect = true
+					}
+				}
+			}
+			if(userFound) {
+				if(passwordCorrect) {
+					$ocModal.close();
+					$scope.mainData.user.loggedIn = true;
+					$scope.mainData.user.loggedInUserName = jQuery('input[name="userName"]').val();
+					mainDataService.saveMainData($scope.mainData);
+				}
+				else {
+					alert('password is incorrect')
+				}
+			}
+			else {
+				alert('user is incorrect')
+			}
+		}
+	}
+	this.logOut = function($scope) {
+		$scope.mainData.user.loggedIn = false;
+		$scope.mainData.user.loggedInUserName = null;
+		mainDataService.saveMainData($scope.mainData);
+	}
+}]);
