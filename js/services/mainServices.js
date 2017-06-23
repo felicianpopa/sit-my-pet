04070cf5ef8efaca8eca8bdaf94d0ba484379690
Felician Popa <felician.popa@freshbyteinc.com>
2017-06-23 14:21:18 +0300

    Added a custom alert box for alert messages.

--- js/services/mainServices.js
+++ js/services/mainServices.js
@@ -1,18 +1,26 @@
 // Load/save the main data.
-app.service('mainDataService', function($http){
+app.service('mainDataService',['$ocModal', '$http', function($ocModal, $http){
 	this.loadMainData = function() {
 		return $http.get('/JSON/mainData.json').then(function (response){
 			return response;
 		});
 	}
-	this.saveMainData = function(dataToSave) {
+	this.saveMainData = function(dataToSave, scope) {
 		$http.post('saveJson.php', dataToSave).then(function(data) {
-	      	alert('Data saved')
+			$ocModal.open({
+				url: 'templates/alertBox.html',
+				controller: 'mainCtrl',
+				init: {
+					alertText: 'data saved',
+					alertType: 'text-success'
+				}
+			});
 	    });
 	}
-});
+}]);
 
-app.service('processUserData',['mainDataService', '$ocModal', function(mainDataService, $ocModal){
+app.service('processUserData',['mainDataService', '$ocModal',
+	function(mainDataService, $ocModal){
 	this.addNewUser = function(newUser, $scope) {
 		if($scope.newUser.userPassword !== $scope.newUser.confirmUserPassword) {
 			$scope.newUserForm.$valid = false;
@@ -25,7 +33,7 @@ app.service('processUserData',['mainDataService', '$ocModal', function(mainDataS
 			$scope.formNotValid = false;
 			var newUserName = $scope.newUser.userName
 			$scope.mainData.usersList[newUserName] = newUser;
-			mainDataService.saveMainData($scope.mainData);
+			mainDataService.saveMainData($scope.mainData, $scope);
 		}
 		else{
 			$scope.formNotValid = true;
@@ -35,7 +43,14 @@ app.service('processUserData',['mainDataService', '$ocModal', function(mainDataS
 		var userFound = false;
 		var passwordCorrect = false;
 		if(angular.equals($scope.mainData.usersList, {})) {
-			alert('nu exista nici un user');
+			$ocModal.open({
+				url: 'templates/alertBox.html',
+				controller: 'mainCtrl',
+				init: {
+					alertText: 'There are no users in the database',
+					alertType: 'text-danger'
+				}
+			});
 			$ocModal.close();
 		}
 		else {
@@ -53,20 +68,34 @@ app.service('processUserData',['mainDataService', '$ocModal', function(mainDataS
 					$ocModal.close();
 					$scope.mainData.user.loggedIn = true;
 					$scope.mainData.user.loggedInUserName = jQuery('input[name="userName"]').val();
-					mainDataService.saveMainData($scope.mainData);
+					mainDataService.saveMainData($scope.mainData, $scope);
 				}
 				else {
-					alert('password is incorrect')
+					$ocModal.open({
+						url: 'templates/alertBox.html',
+						controller: 'mainCtrl',
+						init: {
+							alertText: 'The password is incorrect',
+							alertType: 'text-danger'
+						}
+					});
 				}
 			}
 			else {
-				alert('user is incorrect')
+				$ocModal.open({
+					url: 'templates/alertBox.html',
+					controller: 'mainCtrl',
+					init: {
+						alertText: 'This user does not exist',
+						alertType: 'text-danger'
+					}
+				});
 			}
 		}
 	}
 	this.logOut = function($scope) {
 		$scope.mainData.user.loggedIn = false;
 		$scope.mainData.user.loggedInUserName = null;
-		mainDataService.saveMainData($scope.mainData);
+		mainDataService.saveMainData($scope.mainData, $scope);
 	}
 }]);
